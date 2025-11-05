0e495b174638192f9237809bff6e334c744bf58b
MohamadSolkhanNawawi <mohamadsolkhan558@gmail.com>
2025-11-06 06:50:33 +0700

    chore(setup): initialize Next.js + Tailwind + Shadcn + Supabase client

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <main className="min-h-screen flex items-center justify-center bg-gray-50">
+            <h1 className="text-4xl font-bold text-green-600">
+                Tailwind CSS aktif ✅ (v3.4.13)
+            </h1>
+        </main>
+    );
+}
